2efd428c7786bf123003c66e4fa89da80d992184
Yeseo Lee <6h15m.develop@gmail.com>
2021-08-24 16:04:50 +0900

    [Backup]

--- back/index.js
+++ back/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
 const PORT = process.env.PORT || 3001;
 
 const router = require('./router');
@@ -11,7 +13,26 @@ const server = http.createServer(app);
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-  console.log('We have a new connection!');
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.emit('message', { user: 'admin', text: `${user.name}님, ${user.room}에 오신 것을 환영합니다.` });
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}님이 입장하셨습니다!` });
+
+    socket.join(user.room);
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  });
 
   socket.on('disconnect', () => {
     console.log('User had left!');
--- front/src/components/Chat/Chat.js
+++ front/src/components/Chat/Chat.js
@@ -1,7 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+import './Chat.scss';
 
-const Chat = () => {
-  return <h1>Chat</h1>;
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = 'localhost:3000';
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+    setName(name);
+    setRoom(room);
+
+    socket.emit('join', { name, room }, () => {});
+
+    return () => {
+      socket.emit('disconnect');
+
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  };
+
+  console.log(message, messages);
+
+  return (
+    <div className="chat-outer-container">
+      <div className="chat-inner-container">
+        <div className="chat-left-container">
+          <img className="logo" src="img/logo_small.png" alt="cherry talk logo" />
+          <div className="chat-info-container">
+            <h2>정보</h2>
+            <div className="info-container">
+              <h3>방 이름</h3>
+              <div className="info-text">{room}</div>
+            </div>
+            <div className="info-container">
+              <h3>닉네임</h3>
+              <div className="info-text">{name}</div>
+            </div>
+          </div>
+        </div>
+        <div className="chat-right-container">
+          <div className="message-container"></div>
+          <div className="chat-btm-container">
+            <input
+              className="send-message-input"
+              placeholder="메세지 입력"
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+              onKeyPress={(event) => (event.key === 'Enter' ? sendMessage(event) : null)}
+            />
+            <img className="send-btn" src="img/logo_small.png" alt="send button" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
